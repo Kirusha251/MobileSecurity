4a61f25285a9a03645661dbfaf9806a852e5698f
Kirill Achramionok <mtanl8957@gmail.com>
2017-02-11 11:09:04 +0300

    Init commit
    camera2 frame

---
+++ CameraApplication/app/src/main/java/com/example/kirill/camera2videorecord/CameraActivity.java
@@ -0,0 +1,18 @@
+package com.example.kirill.camera2videorecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2VideoFragment.newInstance())
+                    .commit();
+        }
+    }
+}
